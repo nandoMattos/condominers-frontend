7e190c95beb2abe093490ff5327c01b64e4ec391
Luiz Fernando Mattos <luizfernandoamattos@gmail.com>
2023-04-17 12:18:18 -0300

    feat: fix date

--- src/pages/leisure-spaces/RentHistoricCard.tsx
+++ src/pages/leisure-spaces/RentHistoricCard.tsx
@@ -4,17 +4,9 @@ import { CardContainer } from "../requests/RequestCardGeneric";
 
 type props = Partial<RentSpace>;
 
-export default function RentHistoricCard({
-  User,
-  createdAt,
-  day_rent,
-  id,
-  updatedAt,
-}: props) {
+export default function RentHistoricCard({ User, createdAt, day_rent }: props) {
   return (
-    <CardContainer
-      transparent={dayjs(day_rent).format("DD") < dayjs().format("DD")}
-    >
+    <CardContainer transparent={dayjs(day_rent) < dayjs()}>
       <p>{dayjs(day_rent).format("DD/MM/YYYY")}</p>
       <p>Alugado por: {User?.name}</p>
       <p>Data de agendamento: {dayjs(createdAt).format("DD/MM/YYYY")}</p>
